384af953d133dd0941b337dfd3f6d92447ab43b4
VanderChen <vanderchen@outlook.com>
2020-07-16 01:05:36 +0800

    Add deploy scripts

---
+++ deployment/bare machine/deploy_conf.sh
@@ -0,0 +1,3 @@
+scp -oPort=6502 -r server1/conf cm@47.92.80.237:/home/cm/apache-pulsar-2.6.0/
+scp -oPort=6503 -r server2/conf cm@47.92.80.237:/home/cm/apache-pulsar-2.6.0/
+scp -oPort=6504 -r server3/conf cm@47.92.80.237:/home/cm/apache-pulsar-2.6.0/
