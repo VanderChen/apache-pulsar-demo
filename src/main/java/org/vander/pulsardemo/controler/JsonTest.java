a329981bed322311e44cec807b46d1bc08d50055
VanderChen <vanderchen@outlook.com>
2020-10-11 21:15:02 +0800

    Test Spring boot Request

---
+++ src/main/java/org/vander/pulsardemo/controler/JsonTest.java
@@ -0,0 +1,13 @@
+package org.vander.pulsardemo.controler;
+
+public class JsonTest {
+    private String first_key;
+
+    public String getFirst_key() {
+        return first_key;
+    }
+
+    public void setFirst_key(String first_key) {
+        this.first_key = first_key;
+    }
+}
--- src/main/java/org/vander/pulsardemo/controler/PulsarController.java
+++ src/main/java/org/vander/pulsardemo/controler/PulsarController.java
@@ -1,10 +1,16 @@
 package org.vander.pulsardemo.controler;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pulsar")
 public class PulsarController {
-
+    @PostMapping(path = "/posttest", consumes = "text/plain")
+    public void testPostRequest(@RequestBody String input) throws Exception{
+        System.out.print("Get: " + input);
+    }
 }
